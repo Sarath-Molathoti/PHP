5019adbc834f909da6ebc2579469c72f78c9fce8
Sarath Molathoti <43248607+Sarath-Molathoti@users.noreply.github.com>
2018-12-27 01:22:36 +0530

    Update associative_arrays.php

--- associative_arrays.php
+++ associative_arrays.php
@@ -1,6 +1,6 @@
 <?php
 $marks=array("sarath"=>60,"hasan"=>70,"bharath"=>80);
-$marks["pravitha"]=90;
-echo "Pravitha got ".$marks["pravitha"].'<br>';
+$marks["sai"]=90;
+echo "sai got ".$marks["sai"].'<br>';
 print_r($marks);
 ?>
